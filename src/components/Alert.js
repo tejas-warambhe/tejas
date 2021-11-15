22d75ff5fe290f783dd719a4ff853e866c73cb9d
tejas-warambhe <tejaswarambhe@gmail.com>
2021-11-15 14:44:24 +0530

    ok
    io

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -7,12 +7,14 @@ function Alert(props) {
     }
     
     return (
-        props.alert &&
+        <div style={{height:'50px'}}>
+        {props.alert &&
         <div>
             <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
                 <strong>{capitalise(props.alert.type)}</strong> {props.alert.message}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" ></button>
             </div>
+        </div>}
         </div>
     )
 }
